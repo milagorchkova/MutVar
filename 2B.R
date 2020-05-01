9d0e5d66033262c209a7796167fca211e8585d8a
milagorchkova <mgorchkova@outlook.com>
2020-05-01 16:24:30 -0400

    edited comments final version

--- 2B.R
+++ 2B.R
@@ -15,38 +15,41 @@ setwd("C:/Users/User/Documents/Grade - Year 3/eeb397/csvs")
 dir()
 data2B <- read.csv("2B.csv")
 names(data2B)[1]<-paste("species") 
+names(data2B)[2]<-paste("n")
 names(data2B)[3]<-paste("mean")
-
+names(data2B)[4]<-paste("sd")
 str(data2B)
 
 
-#try intializing p as empty data.frame
-p <- vector(mode="list", length=length(data2B$species))
-SD_vector <-vector(mode='list',length=length(data2B$species))
-mean_vector <-vector(mode='list',length=length(data2B$species))
-n_vector <-vector(mode='list',length=length(data2B$species))
+#initialize empty vectors
+SD_vector <-vector(mode='numeric',length=nrow(data2B))
+mean_vector <-vector(mode='numeric',length=nrow(data2B))
+n_vector <-vector(mode='numeric',length=nrow(data2B))
+df_vector <-vector(mode='numeric',length=nrow(data2B))
 
-#create these vecctors
-SD_vector <- as.vector((data2B$SD))
-SD_vector
-
-SD_total <- sd(data2B$mean, na.rm=TRUE)
-SD_total
-
+#create these vectors and update dataframe
+SD_vector<-as.vector(data2B$sd)
 mean_vector <-as.vector(data2B$mean)
-mean_vector
+n_vector <-as.vector(data2B$n)
+df_vector <-as.vector((n_vector-1))
+data2B$df <- data2B$n-1
 
+SD_vector
+mean_vector
+n_vector
+df_vector
+
+#pooled/totals
 mean_total <-mean(data2B$mean)
 mean_total
 
-n_vector <-as.vector(data2B$N)
-n_vector
+SD_total <- sqrt (sum(((data2B$sd)^2) * data2B$df / sum(data2B$df)))
+SD_total
 
-n_total <-sum(data2B$N)
+n_total <-sum(data2B$n)
 n_total
 
-
-asymptotic_test2(k=(nrow(data2B)), n = n_vector,s = SD_vector, x = mean_vector,1)
+asymptotic_test2(k=(nrow(data2B)), n = data2B$n,s = data2B$sd, x = data2B$mean,1)
 #create vectors needed for test of equality: THIS IS WHEN ONLY SUMMARY STATISTICS AVAILABLE
 #OR to compare "one against all"
 #need:
@@ -64,7 +67,7 @@ x_tmp <-vector(mode='numeric',length=2)
 n_tmp[[2]] <- n_total
 s_tmp[[2]] <-SD_total
 x_tmp[[2]] <-mean_total
-one.v.all <-vector(mode='list',length=nrow(data2A))
+one.v.all <-vector(mode='list',length=nrow(data2B))
 
 for (i in 1:(nrow(data2B)))
 {
@@ -77,15 +80,40 @@ for (i in 1:(nrow(data2B)))
 #output list of test statistics and p-valukes
 one.v.all
 
-##after we have the list of test statistics, when result is significant (and variances are different)
-#we must find out if variance is greater or less than overall.
-#For this we need a list of coefficients of variance to see which is greater.
-CV_vector <- vector(mode='numeric',length=length(unique(data2B$species)))
-CV_vector <- (SD_vector/mean_vector)*100
-CV_vector
+
+##make output table to interpret these results
+
+one.v.all <- unlist(one.v.all)
+str(one.v.all)
+
+test_statistics <- vector(mode='numeric',length=nrow(data2B))
+p_values <- vector(mode='numeric',length=nrow(data2B))
+
+for  (i in 1:(nrow(data2B)))
+{
+  test_statistics[[i]] <- one.v.all[[(i+(i-1))]]
+  p_values[[i]] <- one.v.all[[(i+i)]]
+  
+}
+
+test_statistics
+p_values
+
+p_over_five <- p_values > 0.05
+p_over_five
+
+# if p greater than 5 that means variances are SAME as total. (recall we are looking for those that are same or greater than)
+
+##if p is less than five, that means variances are different
+#next step is to figure out if var is greater or less than total
+#compare outputs
+data2B$cv <- (data2B$sd/data2B$mean)*100
 
 CV_total <- (SD_total/mean_total)*100
 CV_total
 
-comparison <- CV_vector > CV_total
-comparison
+CV_greater <- data2B$cv > CV_total
+CV_greater
+
+cv_results <-data.frame(test_statistics,p_values,p_over_five,CV_greater)         
+view(cv_results)      
